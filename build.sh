cf36d1f7a3aad81f8fb42161103b34e0a6bae31f
Bennett <sbennet@lowes.com>
2016-06-14 23:40:18 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 resume="resume.html"
 index="index.html"
-git="/cygdrive/c/Program\ Files/Git/mingw64/bin/git"
+#git="\"/cygdrive/c/Program Files/Git/mingw64/bin/git\""
 
 if [ -f "$resume" ]; then
     rm $resume
@@ -10,7 +10,7 @@ fi
 
 `/usr/bin/make -s html` || (echo "make failed $?"; exit 1)
 
-$git checkout gh-pages
+git checkout gh-pages
 
 cp ./$resume ./$index
 
