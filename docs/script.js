ba342f7efac121f1334053e638e351515206eb28
Scott Bennett <scottb@fastmail.com>
2020-08-18 21:05:08 -0400

    Add a button to switch dark mode theme on and off

--- docs/script.js
+++ docs/script.js
@@ -1,6 +1,17 @@
 const darkmode = "dark-mode";
 const tags = [ "h1", "h2", "h3", "a" ];
 
+function toggleButton() {
+	let btn = document.getElementById("dark-mode-btn");
+	if (btn.classList.contains(darkmode)) {
+		btn.innerHTML = "Dark";
+	}
+	else {
+		btn.innerHTML = "Light";
+	}
+	btn.classList.toggle(darkmode);
+}
+
 function toggleTags(tagName) {
 	let ts = document.getElementsByTagName(tagName);
 	for (let i = 0; i < ts.length; i++) {
@@ -14,6 +25,7 @@ function toggleTags(tagName) {
 function toggleDarkMode() {
 	let element = document.body;
 	element.classList.toggle(darkmode);
+	toggleButton();
 	for (let i = 0; i < tags.length; i++) {
 		toggleTags(tags[i]);
 	}
