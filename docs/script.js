e1088b7a4e144c4cab1ca0c1c4d8e05c6536307d
Scott Bennett <scottb@fastmail.com>
2020-08-18 22:09:28 -0400

    Create the button via JS so it only shows if JS is enabled

--- docs/script.js
+++ docs/script.js
@@ -1,8 +1,24 @@
+const buttonid = "dark-mode-btn";
 const darkmode = "dark-mode";
 const tags = [ "h1", "h2", "h3", "a" ];
 
+/*
+ * Add the dark-mode button if JS is enabled.
+ */
+function addButton() {
+	let btn = document.createElement("button");
+	btn.setAttribute("id", buttonid);
+	btn.setAttribute("type", "button");
+	btn.onclick = toggleDarkMode;
+	let text = document.createTextNode("Dark");
+	btn.appendChild(text);
+	// insert the button above the one-and-only article
+	let mainArticle = document.getElementsByTagName("article")[0];
+	document.body.insertBefore(btn, mainArticle);
+}
+
 function toggleButton() {
-	let btn = document.getElementById("dark-mode-btn");
+	let btn = document.getElementById(buttonid);
 	if (btn.classList.contains(darkmode)) {
 		btn.innerHTML = "Dark";
 	}
